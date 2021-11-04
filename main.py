e779b0b6772f7c2ca22e4ae158e2d22e8cbd09d1
Trieu Le <ltqtrieu.0204@gmail.com>
2021-11-04 21:03:20 +0700

    Initialize tunnel ssh project

---
+++ main.py
@@ -0,0 +1,11 @@
+from pyngrok import ngrok
+import nest_asyncio
+import time
+
+ngrok.set_auth_token("1zYLRQ6gZ9QmoaC2n7R1UV2ibdA_735LmSK8rcoyyVvZKWCeS")
+ngrok_tunnel = ngrok.connect(22, "tcp")
+print('Public URL:', ngrok_tunnel.public_url)
+nest_asyncio.apply()
+ONE_MONTH_IN_SECONDS = 3600*24*30
+while True:
+    time.sleep(ONE_MONTH_IN_SECONDS)
