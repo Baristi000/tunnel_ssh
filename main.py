a0d191155312be654a129f08019458ed65b33954
Trieu Le <ltqtrieu.0204@gmail.com>
2021-11-04 23:27:55 +0700

    update token

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyngrok import ngrok
 import nest_asyncio
 import time
 
-ngrok.set_auth_token("20Sf4RN3rix4bKSTzYV3jWTDycF_2McMxfZyh8Ut3sbcMCFWz")
+ngrok.set_auth_token("20SfHPyEmnzSBKU6ooXYgRl9qCp_5JFDCSxLdRRZMpaZwCJqc")
 ngrok_tunnel = ngrok.connect(22, "tcp")
 print('Public URL:', ngrok_tunnel.public_url)
 nest_asyncio.apply()
